f4033fd9fe05f63ae4883fdca97eff640e5e23e4
apdonaghy <adonaghy@seattleu.edu>
2021-04-18 10:01:54 -0700

    Making code more readable

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satur
 const weekOfTheMonth = ['first week', 'second week', 'third week', 'fourth week']
 
 const monthTemps = [
-
     [56, 78, 42, 33, 56, 23, 98],
 
     [56, 72, 42, 33, 53, 23, 33],
@@ -11,7 +10,6 @@ const monthTemps = [
     [54, 23, 42, 44, 96, 23, 58],
 
     [56, 78, 32, 32, 56, 43, 18],
-
 ]
 
 weekTemps = []
@@ -26,23 +24,26 @@ function highestTemp(weekArr) {
     return highTemp
 }
 
-function hottestWeek(weekArr){
+function hottestWeek(weekArr) {
     let highTemp = 0;
-    for(let day of weekArr){
+    for (let day of weekArr) {
         highTemp += day
     }
     weekTemps.push(highTemp)
 }
 
 for (let temps of monthTemps) {
-   hottestWeek(temps)
+    hottestWeek(temps)
 }
 
-console.log(`The hottest week this month was the ${weekOfTheMonth[weekTemps.indexOf(highestTemp(weekTemps))]}`)
+let week = weekTemps.indexOf(highestTemp(weekTemps))
 
-// console.log(highestTemp(weekTemps))
+
+console.log(`The hottest week this month was the ${weekOfTheMonth[week]}`)
+
+
 
 for (let temps of monthTemps) {
-
-    console.log(`The hottest day for week ${monthTemps.indexOf(temps) + 1} was ${weekdays[temps.indexOf(highestTemp(temps))]} with a high of ${highestTemp(temps)} `)
+    let day = temps.indexOf(highestTemp(temps))
+    console.log(`The hottest day for week ${monthTemps.indexOf(temps) + 1} was ${weekdays[day]} with a high of ${highestTemp(temps)} `)
 }
